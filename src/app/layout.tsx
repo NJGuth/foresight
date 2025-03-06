79542e1fcdcf80f3d24518c1882e2ce07004abe5
Nicholas Guthrie <nicholas@aetherlens.io>
2025-03-07 08:55:14 +1300

    Initial commit
    Install Shadcn
    Working with tailwind v4
    Fixed intellisense with CVA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css";
+import { ThemeProvider } from "@/components/utility/theme-provider";
+import { ThemeHotkey } from "@/components/utility/theme-hotkey";
+import { ScreenSize } from "@/components/utility/screen-size";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +26,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ThemeHotkey />
+          <ScreenSize />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ src/components/utility/scroll-test.tsx
@@ -0,0 +1,19 @@
+export default function ScrollTest() {
+  // Array of background colors to cycle through
+  const bgColors = ["bg-red-300", "bg-blue-300", "bg-green-300"];
+
+  return (
+    <div className="gap-4 grid lg:grid-cols-2">
+      {Array.from({ length: 40 }).map((_, i) => (
+        <div
+          key={i}
+          className={`min-h-[400px] border rounded-lg grid grid-cols-2 gap-4 p-12 ${
+            bgColors[i % bgColors.length]
+          }`}
+        >
+          Page content {i + 1}
+        </div>
+      ))}
+    </div>
+  );
+}
