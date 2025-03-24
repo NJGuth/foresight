008360ce96d7c6556e53640f17091e9a199da02b
Nicholas Guthrie <nicholas@aetherlens.io>
2025-03-25 07:56:08 +1300

    remove link

--- src/app/(main)/genomes/page.tsx
+++ src/app/(main)/genomes/page.tsx
@@ -1,6 +1,5 @@
 import { ValuesRadar } from "@/components/charts/radar-chart";
 import { ValueData } from "@/components/data/scores";
-import Link from "next/link";
 
 export default function ProjectsPage() {
   return (
